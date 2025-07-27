baae4a2de44a763caaad98e9dd102fc16ee3784f
Piotr Morawski <piotrmorawski90@gmail.com>
2025-07-27 21:06:12 +0200

    chore: fix setup

--- example-app-2/main.go
+++ example-app-2/main.go
@@ -8,6 +8,7 @@ import (
 const version = "1.0.0"
 
 func main() {
+	fmt.Println("Starting example-app-2...")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello from example-app-2! version %s", version)
 	})
